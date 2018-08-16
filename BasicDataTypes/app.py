5bebe8311307ae7eba65cd09bda4fd9de923b943
Vladislav Cernega <vladislav@walkcat.net>
2018-08-16 03:34:25 +0100

    DataTypes

---
+++ BasicDataTypes/app.py
@@ -0,0 +1,32 @@
+# Basic Data Type
+
+# String
+typeString = "Hello World"
+print(typeString)
+
+# Integer
+typeInteger = 10
+typeInteger = typeInteger + 1
+print(typeInteger)
+
+# Float
+typeFloat = 10.5
+
+# Boolean
+typeBoolean = True
+
+# List
+typeList = [1, 2, 3, 4, 5]
+typeList.append(6)
+
+# Tuple
+typeTuple = (1, 2, 3, 4, 5)
+
+# Dictionary
+typeDictionary = {"name": "Vladislav", "age": 15, "hobby": "programming"}
+
+# Set
+typeSet = {1, 2, 3, 4, 5}
+typeSet.add(6)
+
+
