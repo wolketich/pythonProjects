46e395d52eee09104a58a965eaa9b12c7c61d1a6
Vladislav Cernega <vladislav@walkcat.net>
2018-08-30 08:44:44 +0100

    Commit

---
+++ Projects/portScanner/app.py
@@ -0,0 +1,28 @@
+from socket import *
+import time
+startTime = time.time()
+
+from socket import *
+import time
+
+def port_scanner(target):
+    t_IP = gethostbyname(target)
+    print ('Starting scan on host: ', t_IP)
+
+    for i in range(50, 500):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        conn = s.connect_ex((t_IP, i))
+        if(conn == 0) :
+            print ('Port %d: OPEN' % (i,))
+        s.close()   
+
+def main():
+    target = input('Enter the host to be scanned: ')
+    startTime = time.time()
+    port_scanner(target)
+    print('Time taken:', time.time() - startTime)
+
+if __name__ == '__main__':
+    main()
+
