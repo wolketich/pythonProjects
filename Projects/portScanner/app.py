02a0604caeaacef53afe0237d33cc166a9e8300d
Vladislav Cernega <vladislav@walkcat.net>
2018-08-30 08:44:44 +0100

    Commit

--- Projects/portScanner/app.py
+++ Projects/portScanner/app.py
@@ -1,24 +1,65 @@
-from socket import *
+import socket
 import time
+import concurrent.futures
+
+# Constant for the number of maximum threads to use in the port scanner
+MAX_THREADS = 100
+
+def scan_port(target, port):
+    """
+    Try connecting to the specified port on the target host.
+
+    :param target: String host name or IP address
+    :param port: Integer port number
+    :return: True if the port is open, False otherwise
+    """
+    try:
+        # Define socket object with timeout
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)
+            # Try connecting to the port
+            s.connect((target, port))
+            return port
+    except:
+        return None
 
 def port_scanner(target):
-    target_IP = gethostbyname(target)
-    print ('Starting scan on host: ', target_IP)
+    """
+    Scan the target host for open ports.
 
-    for i in range(50, 500):
-        s = socket(AF_INET, SOCK_STREAM)
+    :param target: String host name or IP address
+    """
+    target_IP = socket.gethostbyname(target)
+    print(f'Starting scan on host: {target_IP}')
 
-        conn = s.connect_ex((target_IP, i))
-        if(conn == 0) :
-            print ('Port %d: OPEN' % (i,))
-        s.close()   
+    # To keep track of the open ports
+    open_ports = []
+    
+    # We'll use threading to speed up the scanning process
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+        # Start the scan operations and mark each future with its port
+        future_to_port = {executor.submit(scan_port, target_IP, port): port for port in range(50, 500)}
+        for future in concurrent.futures.as_completed(future_to_port):
+            port = future_to_port[future]
+            if future.result():
+                open_ports.append(port)
+
+    # Print open ports
+    if open_ports:
+        open_ports.sort()
+        for port in open_ports:
+            print(f'Port {port}: OPEN')
+    else:
+        print("No open ports found.")
 
 def main():
     target = input('Enter the host to be scanned: ')
-    startTime = time.time()
-    port_scanner(target)
-    print('Time taken:', time.time() - startTime)
+    if target:
+        startTime = time.time()
+        port_scanner(target)
+        print(f'Time taken: {time.time() - startTime:.2f} seconds')
+    else:
+        print("Invalid target host. Please try again.")
 
 if __name__ == '__main__':
     main()
-
