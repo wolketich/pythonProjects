b0d88b67a4a3553365c882d16ef85d2eba10ae4b
Vladislav Cernega <vladislav@walkcat.net>
2018-09-17 01:49:49 +0100

    Commit

--- Sorting Alghoritms/heapSort.py
+++ Sorting Alghoritms/heapSort.py
@@ -1,1 +1,21 @@
-pass
+# Algorithm: Heap Sort
+
+# Import the random module
+import random
+
+def sort(arr):
+    pass
+
+
+def main():
+    # Generate a random array of N elements
+    array_size = int(input("Enter the size of the array: "))
+    arr = [random.randint(1, 100) for i in range(array_size)]
+    print(f"Unsorted array: {arr}")
+
+    # Sort the array and print the result
+    
+    print(f"Sorted array: {arr}")
+
+if __name__ == "__main__":
+    main()
