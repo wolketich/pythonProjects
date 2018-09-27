5f1515d52db3fc56c90c60df2ad194b3bd6848d4
Vladislav Cernega <vladislav@walkcat.net>
2018-09-27 10:37:07 +0100

    Commit on 2018-09-27 10:37:07

---
+++ Searching Algorithms/binarySearch.py
@@ -0,0 +1,18 @@
+def binary_search(arr, target):
+    """
+    Perform binary search on a sorted array to find the index of the target value.
+    If the target value is not in the array, return -1.
+    """
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
