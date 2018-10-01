fabbdfde14906a1bb1eb7ace8a0cee659907a8a2
Vladislav Cernega <vladislav@walkcat.net>
2018-10-01 21:11:41 +0100

    Commit on 2018-10-1 21:11:41

--- Searching Algorithms/depthFirstSearch.py
+++ Searching Algorithms/depthFirstSearch.py
@@ -1,2 +1,9 @@
-Hello
-
+# Define the depth first search function
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+    return visited
