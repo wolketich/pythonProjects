ad94e20d53c915b786068b0db048d899c06517db
Vladislav Cernega <vladislav@walkcat.net>
2018-10-02 22:49:19 +0100

    Commit on 2018-10-2 22:49:19

--- Searching Algorithms/depthFirstSearch.py
+++ Searching Algorithms/depthFirstSearch.py
@@ -7,3 +7,13 @@ def depth_first_search(graph, start, visited=None):
     for next_node in graph[start] - visited:
         dfs(graph, next_node, visited)
     return visited
+
+def generate_graph():
+    graph = {}
+    graph['A'] = set(['B', 'C'])
+    graph['B'] = set(['A', 'D', 'E'])
+    graph['C'] = set(['A', 'F'])
+    graph['D'] = set(['B'])
+    graph['E'] = set(['B', 'F'])
+    graph['F'] = set(['C', 'E'])
+    return graph
